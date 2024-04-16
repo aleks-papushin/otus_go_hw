1c8163c64ea00a62c321941aeaa4fc62b5eec6b3
Aleksey Papushin <aapapus1@mts.ru>
2024-04-17 00:44:43 +0300

    Refactored

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -8,8 +8,7 @@ import (
 const topN = 10
 
 func Top10(s string) []string {
-	// парсим текст в мапу типа map[string]int (слово:частота)
-	quantityMap := parseTextIntoQuantityMap(s)
+	quantityMap := parseTextIntoQuantityMap(s) // парсим текст в мапу типа map[string]int (слово:частота)
 
 	// преобразуем quantityMap в мапу типа map[int][]string (n:массив строк со словами, встречающимися n раз)
 	// и попутно определяем какова максимальная частота встречаемости
@@ -24,11 +23,9 @@ func Top10(s string) []string {
 func parseTextIntoQuantityMap(s string) map[string]int {
 	wordsArray := strings.Fields(s)
 	quantityMap := make(map[string]int)
-
 	for _, w := range wordsArray {
 		quantityMap[w]++
 	}
-
 	return quantityMap
 }
 
@@ -36,15 +33,9 @@ func projectIntoNumStringMap(quantityMap map[string]int) (map[int][]string, int)
 	numStrMap := make(map[int][]string)
 	maxQ := 0 // храним здесь максимальную встреченную частоту
 	for k, v := range quantityMap {
-		if _, ok := numStrMap[v]; ok {
-			slc := numStrMap[v]
-			slc = append(slc, k)
-			numStrMap[v] = slc
-		} else {
-			numStrMap[v] = []string{k}
-			if v > maxQ {
-				maxQ = v
-			}
+		numStrMap[v] = append(numStrMap[v], k)
+		if v > maxQ {
+			maxQ = v
 		}
 	}
 	return numStrMap, maxQ
@@ -52,16 +43,12 @@ func projectIntoNumStringMap(quantityMap map[string]int) (map[int][]string, int)
 
 func takeTopNSorted(maxQ int, numStrMap map[int][]string) []string {
 	var outSlc []string
-	sum := 0
-	for k := maxQ; k > 0 && sum < topN; k-- {
+	for k := maxQ; k > 0; k-- {
 		if ar, ok := numStrMap[k]; ok {
 			sort.Strings(ar)
-			for _, w := range ar {
-				outSlc = append(outSlc, w)
-				sum++
-				if sum >= topN {
-					break
-				}
+			outSlc = append(outSlc, ar...)
+			if len(outSlc) > topN {
+				return outSlc[:topN]
 			}
 		}
 	}
