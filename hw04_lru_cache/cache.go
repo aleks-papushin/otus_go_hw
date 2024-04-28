104a5350f9d1e4711271f83398598e9c58ebc323
Aleksey Papushin <aapapus1@mts.ru>
2024-04-29 00:28:33 +0300

    HW04. Fixed linter errors

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -2,8 +2,8 @@ package hw04lrucache
 
 type Key string
 
-// будем везде записывать в значение и сам ключ, чтобы иметь возможность удалять из мапы за O(1)
-// в противном случае, удаляя элемент из очереди, пришлось бы по значению искать его в мапе полным обходом мапы
+// Будем везде записывать в значение и сам ключ, чтобы иметь возможность удалять из мапы за O(1)
+// в противном случае, удаляя элемент из очереди, пришлось бы по значению искать его в мапе полным обходом мапы.
 type lruValue struct {
 	key   Key
 	value interface{}
@@ -34,32 +34,33 @@ func (c *lruCache) Set(k Key, v interface{}) bool {
 		key:   k,
 		value: v,
 	}
+
 	if item, ok := c.items[k]; ok { // если элемент в словаре - обновить и переместить в начало очереди
 		item.Value = value
 		c.queue.MoveToFront(item)
 		return ok
-	} else { // если нет, то добавить и поместить в начало,
-		newItem := c.queue.PushFront(value)
-		c.items[k] = newItem
-
-		// при этом, если размер очереди становится больше емкости кэша,
-		// то удалить последний элемент из очереди и его значение из словаря
-		if c.queue.Len() > c.capacity {
-			lastItem := c.queue.Back()
-			c.queue.Remove(lastItem)
-			delete(c.items, lastItem.Value.(lruValue).key)
-		}
-		return false
 	}
+
+	// если нет, то добавить и поместить в начало,
+	newItem := c.queue.PushFront(value)
+	c.items[k] = newItem
+
+	// при этом, если размер очереди становится больше емкости кэша,
+	// то удалить последний элемент из очереди и его значение из словаря
+	if c.queue.Len() > c.capacity {
+		lastItem := c.queue.Back()
+		c.queue.Remove(lastItem)
+		delete(c.items, lastItem.Value.(lruValue).key)
+	}
+	return false
 }
 
 func (c *lruCache) Get(k Key) (interface{}, bool) {
 	if item, ok := c.items[k]; ok {
 		c.queue.MoveToFront(item)
 		return item.Value.(lruValue).value, ok
-	} else {
-		return nil, false
 	}
+	return nil, false
 }
 
 func (c *lruCache) Clear() {
