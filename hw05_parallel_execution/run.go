1ce01c617a72e921b2570e6b158e699de9bc667e
Aleksey Papushin <aleks.papushin@gmail.com>
2024-05-25 18:21:07 +0300

    Hw05 parallel execution (#5)
    * HW05. First implementation. Tests passes but there are fails running 'go test -v -race -count=100 .'
    
    * HW05. go.mod fixed, added another couple of tests, fixed data race
    
    * HW05. Added another test
    
    ---------
    
    Co-authored-by: Aleksey Papushin <aapapus1@mts.ru>

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,8 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +12,46 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	wg := sync.WaitGroup{}
+	mu := sync.Mutex{}
+	errCount := int32(0)
+
+	wg.Add(n)
+	for i := 0; i < n; i++ {
+		go func() {
+			defer wg.Done()
+			worker(&tasks, &mu, m, &errCount)
+		}()
+	}
+
+	wg.Wait()
+
+	if errCount >= int32(m) {
+		return ErrErrorsLimitExceeded
+	}
+
 	return nil
+}
+
+func worker(tasks *[]Task, mu *sync.Mutex, maxErr int, errCount *int32) {
+	for {
+		mu.Lock()
+		if len(*tasks) == 0 {
+			mu.Unlock()
+			return
+		}
+
+		t := (*tasks)[0]
+		*tasks = (*tasks)[1:]
+		mu.Unlock()
+
+		err := t()
+		if err != nil {
+			atomic.AddInt32(errCount, 1)
+		}
+
+		if atomic.LoadInt32(errCount) >= int32(maxErr) {
+			return
+		}
+	}
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -67,4 +67,94 @@ func TestRun(t *testing.T) {
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
 	})
+
+	t.Run("test when tasks number is less than n", func(t *testing.T) {
+		n := 50
+		m := 1
+		tasksNumber := int32(n / 2)
+		completionCounter := int32(0)
+
+		task := func() error {
+			atomic.AddInt32(&completionCounter, 1)
+			return nil
+		}
+		tasks := make([]Task, 0, tasksNumber)
+		for i := 0; i < cap(tasks); i++ {
+			tasks = append(tasks, task)
+		}
+		err := Run(tasks, n, m)
+
+		require.NoError(t, err)
+		require.Equal(t, tasksNumber, completionCounter)
+	})
+
+	t.Run("if m=0 then error limit exceeded", func(t *testing.T) {
+		n := 10
+		m := 0
+
+		task := func() error {
+			return nil
+		}
+		tasks := make([]Task, 0, 50)
+		for i := 0; i < cap(tasks); i++ {
+			tasks = append(tasks, task)
+		}
+		err := Run(tasks, n, m)
+
+		require.ErrorIs(t, err, ErrErrorsLimitExceeded)
+	})
+
+	t.Run("test concurrency without time.Sleep", func(t *testing.T) {
+		// n = 1. Get time (t1) after running 5000 simple tasks
+		n := 1
+		task := func() error {
+			for a := 1; a < 100; a++ {
+				x := rand.Intn(a)
+				x *= rand.Intn(a)
+				x /= rand.Intn(a) + 1
+
+				if x != x-1 { // Workaround for linter which doesn't allow last x result to be unused
+					return nil
+				}
+			}
+			return nil
+		}
+		tasks := make([]Task, 0, 5000)
+		for i := 0; i < cap(tasks); i++ {
+			tasks = append(tasks, task)
+		}
+
+		start := time.Now()
+		err := Run(tasks, n, 1)
+		t1 := time.Since(start)
+
+		require.NoError(t, err)
+
+		// n = 2. Test t2 less than t1 * 2
+		n = 2
+		start = time.Now()
+		err = Run(tasks, n, 1)
+		t2 := time.Since(start)
+
+		require.NoError(t, err)
+		require.Less(t, t2, t1*2)
+
+		// n = 10. Test t3 less than t1 * 10
+		n = 10
+		start = time.Now()
+		err = Run(tasks, n, 1)
+		t3 := time.Since(start)
+
+		require.NoError(t, err)
+		require.Less(t, t3, t1*10)
+
+		// n = 5000. Test t5000 less than t1 * 10
+		n = 5000
+		start = time.Now()
+		err = Run(tasks, n, 1)
+		t5000 := time.Since(start)
+
+		require.NoError(t, err)
+		require.Less(t, t5000, t1*10) // compare with t1*10 (just 10, not 5000)
+	})
 }
