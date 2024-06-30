62e83f62b998303006f06bcfe2cac4bac8b6acff
Aleksey Papushin <aapapus1@mts.ru>
2024-07-01 01:03:39 +0300

    HW08. Added unit-tests

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,61 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	dir := os.TempDir()
+	defer os.RemoveAll(dir)
+
+	testCases := []struct {
+		name          string
+		varName       string
+		varValue      string
+		expectedValue string
+	}{
+		{
+			name:          "Test simple",
+			varName:       "VAR1",
+			varValue:      "value1",
+			expectedValue: "value1",
+		},
+		{
+			name:          "Test spaces",
+			varName:       "VAR2",
+			varValue:      "value2 with spaces",
+			expectedValue: "value2 with spaces",
+		},
+		{
+			name:          "Test tab at the end",
+			varName:       "VAR3",
+			varValue:      "value3\t",
+			expectedValue: "value3",
+		},
+		{
+			name:          "Test terminal 0",
+			varName:       "VAR3",
+			varValue:      "value3\x00with\x00newlines",
+			expectedValue: "value3\nwith\nnewlines",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			os.WriteFile(dir+"/"+tc.varName, []byte(tc.varValue), 0600)
+			env, _ := ReadDir(dir)
+			envValue, ok := env[tc.varName]
+
+			require.True(t, ok, fmt.Sprintf("Variable %s is missing", tc.varName))
+			require.Equal(
+				t,
+				envValue.Value,
+				tc.expectedValue,
+				fmt.Sprintf("Variable %s has wrong value, got %s, expected %s", tc.varName, envValue.Value, tc.varValue))
+		})
+	}
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,19 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	env := Environment{
+		"TEST_ENV_VAR": EnvValue{
+			Value: "test_value",
+		},
+	}
+	cmd := []string{"./test.sh"}
+	returnCode := RunCmd(cmd, env)
+	require.Equal(t, returnCode, 0, fmt.Sprintf("Unexpected return code %d", returnCode))
 }
